b8f217d5168d6f97ac64f6a85defb990d8015b15
THE PROGRAMMER <142110420+Itxxwasi@users.noreply.github.com>
2024-10-12 03:58:35 +0100

    WASI

---
+++ wasimain/plugin/ytv.js
@@ -0,0 +1,224 @@
+function _0x413e(_0x3b654f,_0xf75f07){const _0x15de6b=_0x15de();return _0x413e=function(_0x413ec1,_0x5e61f2){_0x413ec1=_0x413ec1-0x18e;let _0x4e2b4c=_0x15de6b[_0x413ec1];return _0x4e2b4c;},_0x413e(_0x3b654f,_0xf75f07);}(function(_0x354622,_0x1ac114){const _0x40b0a1=_0x413e,_0x5c6d95=_0x354622();while(!![]){try{const _0x449011=-parseInt(_0x40b0a1(0x1bb))/0x1+parseInt(_0x40b0a1(0x197))/0x2*(-parseInt(_0x40b0a1(0x1b5))/0x3)+-parseInt(_0x40b0a1(0x1dd))/0x4+parseInt(_0x40b0a1(0x1a2))/0x5*(parseInt(_0x40b0a1(0x1a9))/0x6)+-parseInt(_0x40b0a1(0x1c4))/0x7+parseInt(_0x40b0a1(0x1d8))/0x8*(-parseInt(_0x40b0a1(0x192))/0x9)+-parseInt(_0x40b0a1(0x1e9))/0xa*(-parseInt(_0x40b0a1(0x195))/0xb);if(_0x449011===_0x1ac114)break;else _0x5c6d95['push'](_0x5c6d95['shift']());}catch(_0x5ad5ff){_0x5c6d95['push'](_0x5c6d95['shift']());}}}(_0x15de,0x5c76d));import _0x27a4f0 from'ytdl-core';function _0x15de(){const _0x5ba4fa=['templateButtonReplyMessage',')\x20-\x20','parse','24147890ovJLYM','toFixed','single_select','message','React','concat','includes','nativeFlowResponseMessage','Message','5902731LhVZwY','\x0aDuration:\x20','♂️\x20Available\x20Qualities','11nNCRse','toString','2cDmhOs','from','toLowerCase','reply','waUploadToServer','slice','interactiveResponseMessage','key','\x0a*👍\x20Likes:*\x20','filterFormats','sendMessage','280lHJgSB','\x20GB','author','videoDetails','\x0aSize:\x20','\x20KB','replace','38268XLvtLi','sender','create','InteractiveMessage','push','ytv','\x0a*🥸Views:*\x20','videoId','NativeFlowMessage','title','Title:\x20','Error\x20fetching\x20video\x20details.','1498299ZHfTWZ','\x0a\x0a>\x20Powered\x20by\x20WASI-MD-V3�','formats','startsWith','end','size','627634yaQcyk','Header','\x0aUpload\x20Date:\x20','lengthSeconds','padStart','length','getInfo','all','\x0aLikes:\x20','3531794kShRhz','​🇵​​🇱​​🇪​​🇦​​🇸​​🇪​\x20​🇵​​🇷​​🇴​​🇻​​🇮​​🇩​​🇪​\x20​🇦​\x20​🇻​​🇦​​🇱​​🇮​​🇩​\x20​🇾​​🇴​​🇺​​🇹​​🇺​​🇧​​🇪​\x20​🇺​​🇷​​🇱​.','getDate','name','video/mp4','stringify','Error\x20fetching\x20video\x20details:','\x0a*🏮\x20Duration:*\x20','WASI-MD-V3\x20Video\x20Downloader\x0a*🔍Title:*\x20','viewCount','floor','trim','error','©\x20Powered\x20By\x20WASI-MD-V3','match','contentLength','Body','duration','\x0a*✍️\x20Author:*\x20','selectedId','8aZAwDd','remoteJid','qualityLabel','bitrate','paramsJson','423024syHdBz','Footer','views','relayMessage','body','💡\x20Choose\x20Quality','\x20MB','likes','validateURL'];_0x15de=function(){return _0x5ba4fa;};return _0x15de();}import _0x3776e9,{prepareWAMessageMedia}from'@whiskeysockets/baileys';const {generateWAMessageFromContent,proto}=_0x3776e9,videoMap=new Map();let videoIndex=0x1;const song=async(_0x516e36,_0x26808e)=>{const _0x48983c=_0x413e;let _0x350f45;const _0x31223e=_0x516e36?.['message']?.[_0x48983c(0x1e6)]?.[_0x48983c(0x1d7)],_0x3ea5a7=_0x516e36?.[_0x48983c(0x1ec)]?.[_0x48983c(0x19d)];if(_0x3ea5a7){const _0x16800b=_0x3ea5a7[_0x48983c(0x190)]?.[_0x48983c(0x1dc)];if(_0x16800b){const _0x24d25b=JSON[_0x48983c(0x1e8)](_0x16800b);_0x350f45=_0x24d25b['id'];}}const _0x1dbcc6=_0x350f45||_0x31223e,_0x2dd1c6=_0x516e36[_0x48983c(0x1e1)][_0x48983c(0x1d2)](/^[\\/!#.]/),_0x4057f0=_0x2dd1c6?_0x2dd1c6[0x0]:'/',_0x13d31a=_0x516e36[_0x48983c(0x1e1)][_0x48983c(0x1b8)](_0x4057f0)?_0x516e36[_0x48983c(0x1e1)][_0x48983c(0x19c)](_0x4057f0[_0x48983c(0x1c0)])['split']('\x20')[0x0][_0x48983c(0x199)]():'',_0x30a643=_0x516e36[_0x48983c(0x1e1)][_0x48983c(0x19c)](_0x4057f0[_0x48983c(0x1c0)]+_0x13d31a[_0x48983c(0x1c0)])[_0x48983c(0x1cf)](),_0xf04a46=[_0x48983c(0x1ae)];if(_0xf04a46[_0x48983c(0x18f)](_0x13d31a)){if(!_0x30a643||!_0x27a4f0[_0x48983c(0x1e5)](_0x30a643))return _0x516e36[_0x48983c(0x19a)](_0x48983c(0x1c5));try{await _0x516e36[_0x48983c(0x1ed)]('🔊');const _0x1a9dc5=await _0x27a4f0[_0x48983c(0x1c1)](_0x30a643),_0x44df7b=_0x27a4f0[_0x48983c(0x1a0)](_0x1a9dc5[_0x48983c(0x1b7)],'videoandaudio');if(_0x44df7b['length']===0x0){_0x516e36['reply']('No\x20downloadable\x20formats\x20found.'),await _0x516e36[_0x48983c(0x1ed)]('❌');return;}const _0x3e7748={'title':_0x1a9dc5['videoDetails']['title'],'author':_0x1a9dc5[_0x48983c(0x1a5)][_0x48983c(0x1a4)][_0x48983c(0x1c7)],'views':_0x1a9dc5[_0x48983c(0x1a5)][_0x48983c(0x1cd)],'likes':_0x1a9dc5[_0x48983c(0x1a5)]['likes'],'uploadDate':formatDate(_0x1a9dc5['videoDetails']['uploadDate']),'duration':formatDuration(_0x1a9dc5[_0x48983c(0x1a5)][_0x48983c(0x1be)])},_0x15b2e0=await Promise[_0x48983c(0x1c2)](_0x44df7b['map'](async(_0x25be6b,_0x296d5b)=>{const _0xb2fc3d=_0x48983c,_0xcf5443=videoIndex+_0x296d5b,_0x599da7=_0x25be6b[_0xb2fc3d(0x1d3)]?formatSize(_0x25be6b[_0xb2fc3d(0x1d3)]):'Unknown\x20size';return videoMap['set'](_0xcf5443,{..._0x25be6b,'videoId':_0x1a9dc5[_0xb2fc3d(0x1a5)][_0xb2fc3d(0x1b0)],..._0x3e7748,'size':_0x599da7}),{'header':'','title':_0x25be6b[_0xb2fc3d(0x1da)]+'\x20('+_0x25be6b['container']+_0xb2fc3d(0x1e7)+_0x599da7,'description':'Bitrate:\x20'+_0x25be6b[_0xb2fc3d(0x1db)],'id':'quality_'+_0xcf5443};})),_0x45433c=generateWAMessageFromContent(_0x516e36['from'],{'viewOnceMessage':{'message':{'messageContextInfo':{'deviceListMetadata':{},'deviceListMetadataVersion':0x2},'interactiveMessage':proto[_0x48983c(0x191)][_0x48983c(0x1ac)][_0x48983c(0x1ab)]({'body':proto[_0x48983c(0x191)][_0x48983c(0x1ac)][_0x48983c(0x1d4)][_0x48983c(0x1ab)]({'text':_0x48983c(0x1cc)+_0x3e7748['title']+_0x48983c(0x1d6)+_0x3e7748[_0x48983c(0x1a4)]+_0x48983c(0x1af)+_0x3e7748[_0x48983c(0x1df)]+_0x48983c(0x19f)+_0x3e7748[_0x48983c(0x1e4)]+'\x0a*📆\x20Upload\x20Date:*\x20'+_0x3e7748['uploadDate']+_0x48983c(0x1cb)+_0x3e7748['duration']+'\x0a'}),'footer':proto['Message']['InteractiveMessage'][_0x48983c(0x1de)][_0x48983c(0x1ab)]({'text':_0x48983c(0x1d1)}),'header':proto[_0x48983c(0x191)][_0x48983c(0x1ac)][_0x48983c(0x1bc)]['create']({...await prepareWAMessageMedia({'image':{'url':'https://telegra.ph/file/149eb4625a84a6666958a.jpg'}},{'upload':_0x26808e[_0x48983c(0x19b)]}),'title':'','gifPlayback':!![],'subtitle':'','hasMediaAttachment':![]}),'nativeFlowMessage':proto[_0x48983c(0x191)][_0x48983c(0x1ac)][_0x48983c(0x1b1)][_0x48983c(0x1ab)]({'buttons':[{'name':_0x48983c(0x1eb),'buttonParamsJson':JSON[_0x48983c(0x1c9)]({'title':'🎬\x20Select\x20a\x20video\x20quality','sections':[{'title':_0x48983c(0x194),'highlight_label':_0x48983c(0x1e2),'rows':_0x15b2e0}]})}]}),'contextInfo':{'mentionedJid':[_0x516e36[_0x48983c(0x1aa)]],'forwardingScore':0x3e7,'isForwarded':!![]}})}}},{});await _0x26808e[_0x48983c(0x1e0)](_0x45433c[_0x48983c(0x19e)][_0x48983c(0x1d9)],_0x45433c[_0x48983c(0x1ec)],{'messageId':_0x45433c['key']['id']}),await _0x516e36['React']('✅'),videoIndex+=_0x44df7b[_0x48983c(0x1c0)];}catch(_0x576a70){console['error']('Error\x20processing\x20your\x20request:',_0x576a70),_0x516e36['reply']('Error\x20processing\x20your\x20request.'),await _0x516e36[_0x48983c(0x1ed)]('❌');}}else{if(_0x1dbcc6){const _0x20b8b3=parseInt(_0x1dbcc6[_0x48983c(0x1a8)]('quality_','')),_0x5a4e80=videoMap['get'](_0x20b8b3);if(_0x5a4e80)try{const _0x18089f='https://www.youtube.com/watch?v='+_0x5a4e80[_0x48983c(0x1b0)],_0x528760=_0x27a4f0(_0x18089f,{'format':_0x5a4e80}),_0x3a7810=await streamToBuffer(_0x528760),_0x41138c=_0x5a4e80[_0x48983c(0x1d5)],_0x36457b=_0x5a4e80[_0x48983c(0x1ba)];await _0x26808e[_0x48983c(0x1a1)](_0x516e36[_0x48983c(0x198)],{'video':_0x3a7810,'mimetype':_0x48983c(0x1c8),'caption':_0x48983c(0x1b3)+_0x5a4e80[_0x48983c(0x1b2)]+'\x0aAuthor:\x20'+_0x5a4e80['author']+'\x0aViews:\x20'+_0x5a4e80[_0x48983c(0x1df)]+_0x48983c(0x1c3)+_0x5a4e80[_0x48983c(0x1e4)]+_0x48983c(0x1bd)+_0x5a4e80['uploadDate']+_0x48983c(0x193)+_0x41138c+_0x48983c(0x1a6)+_0x36457b+_0x48983c(0x1b6)},{'quoted':_0x516e36});}catch(_0x49cfe4){console[_0x48983c(0x1d0)](_0x48983c(0x1ca),_0x49cfe4),_0x516e36['reply'](_0x48983c(0x1b4));}else{}}}},formatDuration=_0x5d1ae4=>{const _0xd4808d=_0x413e,_0xcd1012=Math['floor'](_0x5d1ae4/0xe10),_0x1f8ff0=Math[_0xd4808d(0x1ce)](_0x5d1ae4%0xe10/0x3c),_0x52b844=_0x5d1ae4%0x3c;return _0xcd1012+'h\x20'+_0x1f8ff0+'m\x20'+_0x52b844+'s';},formatSize=_0x57330f=>{const _0x278607=_0x413e;if(_0x57330f<0x400)return _0x57330f['toFixed'](0x2)+'\x20B';if(_0x57330f<0x400*0x400)return(_0x57330f/0x400)['toFixed'](0x2)+_0x278607(0x1a7);if(_0x57330f<0x400*0x400*0x400)return(_0x57330f/0x400/0x400)[_0x278607(0x1ea)](0x2)+_0x278607(0x1e3);return(_0x57330f/0x400/0x400/0x400)[_0x278607(0x1ea)](0x2)+_0x278607(0x1a3);},formatDate=_0xd9a5aa=>{const _0xbee1f4=_0x413e,_0x5ce868=new Date(_0xd9a5aa),_0x5396a5=_0x5ce868[_0xbee1f4(0x1c6)]()[_0xbee1f4(0x196)]()[_0xbee1f4(0x1bf)](0x2,'0'),_0x4c9361=(_0x5ce868['getMonth']()+0x1)[_0xbee1f4(0x196)]()[_0xbee1f4(0x1bf)](0x2,'0'),_0x4ecfc5=_0x5ce868['getFullYear']();return _0x5396a5+'-'+_0x4c9361+'-'+_0x4ecfc5;},streamToBuffer=async _0x37984a=>{return new Promise((_0x1f42c4,_0x40355c)=>{const _0x43ff37=_0x413e,_0x4f8b5f=[];_0x37984a['on']('data',_0x544cc5=>_0x4f8b5f[_0x43ff37(0x1ad)](_0x544cc5)),_0x37984a['on'](_0x43ff37(0x1b9),()=>_0x1f42c4(Buffer[_0x43ff37(0x18e)](_0x4f8b5f))),_0x37984a['on']('error',_0x40355c);});};export default song;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+//import ytdl from 'ytdl-core';
+//import yts from 'yt-search';
+import ytdl from 'ytdl-core';
+import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto } = pkg;
+
+const videoMap = new Map();
+let videoIndex = 1; 
+
+const song = async (m, Matrix) => {
+  let selectedListId;
+  const selectedButtonId = m?.message?.templateButtonReplyMessage?.selectedId;
+  const interactiveResponseMessage = m?.message?.interactiveResponseMessage;
+
+  if (interactiveResponseMessage) {
+    const paramsJson = interactiveResponseMessage.nativeFlowResponseMessage?.paramsJson;
+    if (paramsJson) {
+      const params = JSON.parse(paramsJson);
+      selectedListId = params.id;
+    }
+  }
+
+  const selectedId = selectedListId || selectedButtonId;
+
+  const prefixMatch = m.body.match(/^[\\/!#.]/);
+  const prefix = prefixMatch ? prefixMatch[0] : '/';
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const text = m.body.slice(prefix.length + cmd.length).trim();
+  
+  const validCommands = ['ytv'];
+
+  if (validCommands.includes(cmd)) {
+    if (!text || !ytdl.validateURL(text)) {
+      return m.reply('​🇵​​🇱​​🇪​​🇦​​🇸​​🇪​ ​🇵​​🇷​​🇴​​🇻​​🇮​​🇩​​🇪​ ​🇦​ ​🇻​​🇦​​🇱​​🇮​​🇩​ ​🇾​​🇴​​🇺​​🇹​​🇺​​🇧​​🇪​ ​🇺​​🇷​​🇱​.');
+    }
+
+    try {
+      await m.React("🔊");
+
+
+      const info = await ytdl.getInfo(text);
+      const formats = ytdl.filterFormats(info.formats, 'videoandaudio');
+
+      if (formats.length === 0) {
+        m.reply('No downloadable formats found.');
+        await m.React("❌");
+        return;
+      }
+
+      const videoDetails = {
+        title: info.videoDetails.title,
+        author: info.videoDetails.author.name,
+        views: info.videoDetails.viewCount,
+        likes: info.videoDetails.likes,
+        uploadDate: formatDate(info.videoDetails.uploadDate),
+        duration: formatDuration(info.videoDetails.lengthSeconds)
+      };
+
+      const qualityButtons = await Promise.all(formats.map(async (format, index) => {
+        const uniqueId = videoIndex + index;
+        const size = format.contentLength ? formatSize(format.contentLength) : 'Unknown size';
+        videoMap.set(uniqueId, { ...format, videoId: info.videoDetails.videoId, ...videoDetails, size });
+        return {
+          "header": "",
+          "title": `${format.qualityLabel} (${format.container}) - ${size}`,
+          "description": `Bitrate: ${format.bitrate}`,
+          "id": `quality_${uniqueId}` 
+        };
+      }));
+
+      const msg = generateWAMessageFromContent(m.from, {
+        viewOnceMessage: {
+          message: {
+            messageContextInfo: {
+              deviceListMetadata: {},
+              deviceListMetadataVersion: 2
+            },
+            interactiveMessage: proto.Message.InteractiveMessage.create({
+              body: proto.Message.InteractiveMessage.Body.create({
+                text: `WASI-MD-V3 Video Downloader\n*🔍Title:* ${videoDetails.title}\n*✍️ Author:* ${videoDetails.author}\n*🥸Views:* ${videoDetails.views}\n*👍 Likes:* ${videoDetails.likes}\n*📆 Upload Date:* ${videoDetails.uploadDate}\n*🏮 Duration:* ${videoDetails.duration}\n`
+              }),
+              footer: proto.Message.InteractiveMessage.Footer.create({
+                text: "© Powered By WASI-MD-V3"
+              }),
+              header: proto.Message.InteractiveMessage.Header.create({
+                ...(await prepareWAMessageMedia({ image: { url: `https://telegra.ph/file/149eb4625a84a6666958a.jpg` } }, { upload: Matrix.waUploadToServer })),
+                title: "",
+                gifPlayback: true,
+                subtitle: "",
+                hasMediaAttachment: false 
+              }),
+              nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+                buttons: [
+                  {
+                    name: "single_select",
+                    buttonParamsJson: JSON.stringify({
+                      title: "🎬 Select a video quality",
+                      sections: [
+                        {
+                          title: "♂️ Available Qualities",
+                          highlight_label: "💡 Choose Quality",
+                          rows: qualityButtons
+                        },
+                      ]
+                    })
+                  },
+                ],
+              }),
+              contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 999,
+                isForwarded: true,
+              }
+            }),
+          },
+        },
+      }, {});
+
+      await Matrix.relayMessage(msg.key.remoteJid, msg.message, {
+        messageId: msg.key.id
+      });
+      await m.React("✅");
+
+      videoIndex += formats.length;
+    } catch (error) {
+      console.error("Error processing your request:", error);
+      m.reply('Error processing your request.');
+      await m.React("❌");
+    }
+  } else if (selectedId) {
+    const key = parseInt(selectedId.replace('quality_', ''));
+    const selectedFormat = videoMap.get(key);
+
+    if (selectedFormat) {
+      try {
+        const videoUrl = `https://www.youtube.com/watch?v=${selectedFormat.videoId}`;
+        const videoStream = ytdl(videoUrl, { format: selectedFormat });
+        const finalVideoBuffer = await streamToBuffer(videoStream);
+
+        const duration = selectedFormat.duration;
+        const size = selectedFormat.size;
+
+        await Matrix.sendMessage(m.from, {
+          video: finalVideoBuffer,
+          mimetype: 'video/mp4',
+          caption: `Title: ${selectedFormat.title}\nAuthor: ${selectedFormat.author}\nViews: ${selectedFormat.views}\nLikes: ${selectedFormat.likes}\nUpload Date: ${selectedFormat.uploadDate}\nDuration: ${duration}\nSize: ${size}\n\n> Powered by WASI-MD-V3�`
+        }, { quoted: m });
+      } catch (error) {
+        console.error("Error fetching video details:", error);
+        m.reply('Error fetching video details.');
+      }
+    } else {
+    }
+  }
+};
+
+const formatDuration = (seconds) => {
+  const h = Math.floor(seconds / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = seconds % 60;
+  return `${h}h ${m}m ${s}s`;
+};
+
+const formatSize = (size) => {
+  if (size < 1024) return `${size.toFixed(2)} B`;
+  if (size < 1024 * 1024) return `${(size / 1024).toFixed(2)} KB`;
+  if (size < 1024 * 1024 * 1024) return `${(size / 1024 / 1024).toFixed(2)} MB`;
+  return `${(size / 1024 / 1024 / 1024).toFixed(2)} GB`;
+};
+
+const formatDate = (date) => {
+  const d = new Date(date);
+  const day = d.getDate().toString().padStart(2, '0');
+  const month = (d.getMonth() + 1).toString().padStart(2, '0');
+  const year = d.getFullYear();
+  return `${day}-${month}-${year}`;
+};
+
+const streamToBuffer = async (stream) => {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on('data', chunk => chunks.push(chunk));
+    stream.on('end', () => resolve(Buffer.concat(chunks)));
+    stream.on('error', reject);
+  });
+};
+
+export default song;
+*/
